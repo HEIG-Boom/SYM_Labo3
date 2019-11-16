7b757d758881c461eb2bab8e5c5da986a28094f6
Mateo Tutic <mateo@tutic.ch>
2019-11-16 21:02:00 +0100

    Init all activities

--- src/app/src/main/java/ch/heigvd/sym/labo3/MainActivity.java
+++ src/app/src/main/java/ch/heigvd/sym/labo3/MainActivity.java
@@ -2,13 +2,44 @@ package ch.heigvd.sym.labo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+/**
+ * Main activity class used to display the two buttons to start NFC and Barcode activities
+ *
+ * @author Jael Dubey, Loris Gilliand, Mateo Tutic, Luc Wachter
+ * @version 1.0
+ * @since 2019-11-15
+ */
 public class MainActivity extends AppCompatActivity {
+    // Graphics components
+    private Button btnNFC = null;
+    private Button btnBarcode = null;
 
+    /**
+     * Set up buttons used to launch NFC and Barcode activities
+     *
+     * @param savedInstanceState Bundle object containing the activity's previously saved state.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNFC = findViewById(R.id.btnNFC);
+        btnBarcode = findViewById(R.id.btnBarcode);
+
+        // Launch the appropriate activity
+        btnNFC.setOnClickListener((v) -> {
+            Intent intent = new Intent(this, ch.heigvd.sym.labo3.NFCActivity.class);
+            this.startActivity(intent);
+        });
+
+        btnBarcode.setOnClickListener((v) -> {
+            Intent intent = new Intent(this, ch.heigvd.sym.labo3.BarcodeActivity.class);
+            this.startActivity(intent);
+        });
     }
 }
