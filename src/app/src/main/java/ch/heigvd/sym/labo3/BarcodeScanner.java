ce1621e70dcc9cbd03bca1a918c8628d70f39f7c
Loris <loris.gilliand@heig-vd.ch>
2019-11-22 14:36:14 +0100

    scan activity 80%

---
+++ src/app/src/main/java/ch/heigvd/sym/labo3/BarcodeScanner.java
@@ -0,0 +1,47 @@
+package ch.heigvd.sym.labo3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class BarcodeScanner extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        scannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        scannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        scannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        // Log.v("tag", rawResult.getText()); // Prints scan results
+        // Log.v("tag", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        BarcodeActivity.barCodeValue.setText(rawResult.getText());
+        onBackPressed();
+
+        // If you would like to resume scanning, call this method below:
+        //mScannerView.resumeCameraPreview(this);
+    }
+}
